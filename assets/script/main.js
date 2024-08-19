0e650b15f9ee76f8c18ee8574612b3500ac79abe
yuuboukun <pikahikari87@gmail.com>
2024-08-19 20:56:59 +0900

    240815_yuuzou.yamadaのブランチのコミット002

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,2 +1,37 @@
 // カルーセル
-const swiper = new Swiper('.swiper');
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        prevEl: '.my-button-prev',
+        nextEl: '.my-button-next',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        // 767px以下の設定
+        0: {
+            slidesPerView: 1, // 表示するスライド数
+            slidesPerGroup: 1, // 移動するスライド数
+        },
+        // 768px以上の設定
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: window.innerWidth * 0.026667,
+        },
+    },
+});
+
+// アコーディオン
+$('.faq__question').on('click', function (e) {
+    // アンサーを選択
+    var answer = $(this).next();
+
+    // answerを表示・非表示
+    answer.slideToggle();
+    // アイコンの上下反転
+    $(this).find('.bi').toggleClass('rotate');
+});
